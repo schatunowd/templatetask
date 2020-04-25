b13d0d83f863a89af265829e86878db45e2ef7a4
xochyymeret <60923500+xochyymeret@users.noreply.github.com>
2020-04-25 04:56:16 +0300

    stack new 2

--- stack/templatetask/templatetask.cpp
+++ stack/templatetask/templatetask.cpp
@@ -1,106 +1,97 @@
-﻿#pragma once
-#include <iostream>
-#include <stdexcept>
+﻿#include <iostream>
+#include <stack>
+#include <clocale>
 using namespace std;
 
 template <class T>
 class Stack;
-template<class T>
-Stack<T> StackMerge(const Stack<T>& first, const Stack<T>& second);
 
 template<class T>
 class Stack
 {
 private:
-	int size;
-	T* array;
-	int head;
-	bool Empty()
-	{
-		if (head == size)
-			return true;
-		else
-			return false;
-	}
-	bool Full()
-	{
-		if (head == 0)
-			return true;
-		else
-			return false;
-	}
+    T* array;
+    int size;
+    int head;
 public:
-	Stack(int size)
-	{
-		this->size = size;
-		this->array = new T[this->size];
-		this->head = size;
-	}
-	Stack(const Stack& stack)
-	{
-		this->size = stack.size;
-		this->array = new T[this->size];
-		this->head = stack.head;
-	}
-
-	~Stack()
-	{
-		delete[] array;
-	}
-
-	void push(T element)
-	{
-		if (this->Full())
-			throw std::out_of_range("Stack is full!");
-		this->head = this->head - 1;
-		this->array[this->head] = element;
-	}
-
-	T pop()
-	{
-		if (this->Empty())
-			throw std::out_of_range("Stack is empty!");
-		T element = this->array[this->head];
-		this->head = this->head + 1;
-		return element;
-	}
-	friend Stack StackMerge<T>(const Stack<T>& first, const Stack<T>& second);
+    Stack(int Full) 
+    {
+        this->size = Full;
+        this->array = new T[size];
+        this->head = 0;
+    }
+    Stack(const Stack<T>& stack) 
+    {
+        this->head = stack.head;
+        this->array = new T[Stack.size];
+        this->array = stack.array;
+    }
+    void push(T elem)
+    {
+        if (this->head >= this->size)
+            throw out_of_range("Stack is full");
+        array[this->head++] = elem;
+    }
+    void pop()
+    {
+        if (this->head <= 0)
+            throw out_of_range("Stack is empty");
+        this->head--;
+    }
+    void out()
+    {
+        for (int i = 0; i < head; i++)
+            cout << array[i] << " ";
+        cout << endl;
+    }
+    ~Stack()
+    {
+        delete[] array;
+    }
+    void StackMerge(const Stack<T>& second);
 };
-
-template<class T>
-Stack<T> StackMerge(const Stack<T>& first, const Stack<T>& second)
+template<typename T>
+void Stack<T>::StackMerge(const Stack<T>& second)
 {
-	int size = first.size + second.size;
-	Stack<T> stack = Stack<T>(size);
-	int index = first.size;
-	while (index != first.head)
-	{
-		index = index - 1;
-		stack.push(first.array[index]);
-	}
-	index = second.size;
-	while (index != second.head)
-	{
-		index = index - 1;
-		stack.push(second.array[index]);
-	}
-	return stack;
+    T* merge = new T[size + second.size];
+    head = 0;
+    while (head < size) {
+        merge[head] = array[head];
+        head++;
+    }
+    while (head < size + second.size) {
+        merge[head] = second.array[head - size];
+        head++;
+    }
+    array = merge;
 }
 int main()
 {
-	Stack <int> stack1(5);
-	Stack <int> stack2(5);
-	stack1.push(10);
-	stack1.push(2);
-	stack1.push(4);
-	stack1.push(1);
-	stack1.push(15);
-	stack2.push(32);
-	stack2.push(11);
-	stack2.push(12);
-	stack2.push(14);
-	stack2.push(17);
-	stack1.pop();
-	stack2.pop();
-	StackMerge(stack1, stack2);
+    setlocale(0, "");
+    Stack <int> stack1(5);
+    Stack <int> stack2(5);
+    cout << "Стэк 1: ";
+    stack1.push(10);
+    stack1.push(2);
+    stack1.push(4);
+    stack1.push(1);
+    stack1.push(15);
+    stack1.out();
+    cout << "Стэк 2: ";
+    stack2.push(32);
+    stack2.push(11);
+    stack2.push(12);
+    stack2.push(14);
+    stack2.push(17);
+    stack2.out();
+    cout << "Стэк 1 после удаления нового элемента: ";
+    stack1.pop();
+    stack1.out();
+    cout << "Стэк 2 после удаления нового элемента: ";
+    stack2.pop();
+    stack2.out();
+    Stack <int> stack3(10);
+    stack1.StackMerge(stack2);
+    cout << "Новый стэк после слияния: ";
+    stack1.out();
 }
