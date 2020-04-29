7fa7f06eda4c3630256c9ddb86af895862360741
xochyymeret <60923500+xochyymeret@users.noreply.github.com>
2020-04-29 21:32:56 +0300

    Add files via upload

---
+++ Stack(Csharp)/Stack(Csharp)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack_Csharp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5, n2 = 4;
+            StackClass<int> stack1 = new StackClass<int>(n);
+            StackClass<int> stack2 = new StackClass<int>(n);
+            stack1.Push(2);
+            stack1.Push(4);
+            stack1.Push(6);
+            stack1.Push(8);
+            stack1.Push(10);
+            Console.Write("Первый стэк: ");
+            stack1.print(n);
+            stack2.Push(1);
+            stack2.Push(3);
+            stack2.Push(5);
+            stack2.Push(7);
+            stack2.Push(9);
+            Console.Write("Второй стэк: ");
+            stack2.print(n);
+            stack1.Pop();
+            Console.Write("Первый стэк после удаления первого элемента: ");
+            stack1.print(n2);
+            stack2.Pop();
+            Console.Write("Второй стэк после удаления первого элемента: ");
+            stack2.print(n2);
+            Console.Write("Объединение двух стэков: ");
+            StackClass<int> stack3 = new StackClass<int>(stack1, stack2);
+            stack3.print(8);
+        }
+    }
+}
---
+++ Stack(Csharp)/Stack(Csharp)/StackClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack_Csharp_
+{
+    class StackClass<T>
+    {
+        public Stack<T> stack;
+        int l;
+        public StackClass(int length)
+        {
+            if (length > 0)
+            {
+                this.l = length;
+                stack = new Stack<T>();
+            }
+            else
+                throw new ArgumentException();
+        }
+        public StackClass(StackClass<T> s1, StackClass<T> s2)
+        {
+            this.l = s1.stack.Count() + s2.stack.Count();
+            stack = new Stack<T>();
+            Stack<T> st1 = s1.stack;
+            Stack<T> st2 = s2.stack;
+            while (st1.Count() != 0)
+            {
+                Push(st1.First());
+                st1.Pop();
+            }
+            while (st2.Count() != 0)
+            {
+                Push(st2.First());
+                st2.Pop();
+            }
+        }
+        public void Pop()
+        {
+            if (stack.Count > 0)
+                stack.Pop();
+
+        }
+
+        public void print(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(stack.ElementAt(i));
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Push(T element)
+        {
+            stack.Push(element);
+            if (stack.Count() > this.l)
+            {
+                Pop();
+                throw new IndexOutOfRangeException("Переполнение стека");
+            }
+        }
+        ~StackClass() 
+        { 
+        }
+    }
+}
